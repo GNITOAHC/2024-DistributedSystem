4807e4bee5c14890d7d34eb30a30e9f8eb3f13c9
GNITOAHC <chaotingchen10@gmail.com>
2024-06-11 01:56:08 +0800

    feat: mqtt support

--- device/device.js
+++ device/device.js
@@ -1,10 +1,28 @@
+const mqtt = require('mqtt')
 require("dotenv").config();
 const { Board, Proximity, IMU } = require("johnny-five");
+const client = mqtt.connect()
 
 const board = new Board({
   port: process.env.COM_NUM,
   repl: false
 });
+
+function HCSR04_data(cm, time) {
+  return {
+    "value": cm,
+    "time": time
+  }
+}
+
+function MPU6050_data(pitch, roll, yaw, time) {
+  return {
+    "time": time,
+    "pitch": {"rate": pitch.rate, "angle": pitch.angle},
+    "roll": {"rate": roll.rate, "angle": roll.angle},
+    "yaw": {"rate": yaw.rate, "angle": yaw.angle}
+  }
+}
 
 board.on("ready", () => {
   // HC-SR04
@@ -23,6 +41,7 @@ board.on("ready", () => {
   hcsr04.on("data", () => {
     const { centimeters } = hcsr04;
     console.log("  cm  : ", centimeters);
+    client.publish('HCSR04', JSON.stringify(HCSR04_data(centimeters, Date.now())))
   });
 
   mpu6050.on("data", () => {
@@ -30,5 +49,6 @@ board.on("ready", () => {
     console.log("  pitch  : ", pitch);
     console.log("  roll   : ", roll);
     console.log("  yaw    : ", yaw);
+    client.publish('MPU6050', JSON.stringify(MPU6050_data(pitch, roll, yaw, Date.now())))
   });
 });
