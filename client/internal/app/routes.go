94cfad22f706e0cd8db5ca4b84e0e4121a8512fc
GNITOAHC <chaotingchen10@gmail.com>
2024-06-16 20:41:35 +0800

    feat: Client display

--- client/internal/app/routes.go
+++ client/internal/app/routes.go
@@ -16,12 +16,25 @@ func (a *App) LandingRoute(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *App) MPU6050(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/base.html", "templates/mpu6050.html"))
-	err := tmpl.Execute(w, nil)
+	startTime := r.URL.Query().Get("start")
+	endTime := r.URL.Query().Get("end")
+	if startTime == "" || endTime == "" {
+		log.Println("No start or end time provided")
+		a.tmplExecute(w, []string{"templates/base.html", "templates/mpu6050.html"}, nil)
+		return
+	}
+	data, err := a.helper.GetMPU6050(startTime, endTime)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	return
 }
 
 func (a *App) HCSR04(w http.ResponseWriter, r *http.Request) {
--- client/internal/helper/helper.go
+++ client/internal/helper/helper.go
@@ -21,6 +21,32 @@ func NewHelper() *Helper {
 	}
 }
 
+func (h *Helper) GetMPU6050(start, end string) (map[string]interface{}, error) {
+	requestString := apiURL + "MPU6050?start=" + start + "&end=" + end
+
+	req, err := http.NewRequest("GET", requestString, nil)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	res, err := h.client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	body, err := io.ReadAll(res.Body)
+	log.Println(string(body))
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	var data map[string]interface{}
+	json.Unmarshal(body, &data)
+
+	return data, nil
+}
+
 func (h *Helper) GetHCSR04(start, end string) (map[string]interface{}, error) {
 	requestString := apiURL + "HCSR04?start=" + start + "&end=" + end
 
